61efaafe50a6be6eb057bfc41008ea393986e02b
Salil Wadnerkar <rohshall@gmail.com>
2012-06-10 09:46:48 +0800

    working setup with elasticserver running on localhost:9200

--- hello_es.py
+++ hello_es.py
@@ -3,8 +3,7 @@ from pyes import *
 class Model(object):
 
     def __init__(self):
-      self.conn = ES('127.0.0.1:8888')
-        print self.conn.collect_info()
+      self.conn = ES('127.0.0.1:9200')
         
     def get_message(self):  
-        return self.conn.collect_info()
+      return self.conn.info
